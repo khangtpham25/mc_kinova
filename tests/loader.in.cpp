34be0febe0ca8ec513bbbe1caedbea039cc04f85
Khang Pham <phamthekhang632@gmail.com>
2026-06-11 15:16:02 +0900

    test: include other variations if dependencies are found

--- tests/loader.in.cpp
+++ tests/loader.in.cpp
@@ -7,7 +7,11 @@ int main(int argc, char * argv[])
     return 1;
   }
   mc_rbdyn::RobotLoader::clear();
-  mc_rbdyn::RobotLoader::update_robot_module_path({std::string(argv[1])});
-  auto rm = mc_rbdyn::RobotLoader::get_robot_module(@ROBOT_MODULE_PARAMS @);
+  mc_rbdyn::RobotLoader::update_robot_module_path({std::string(argv[1]), "@MC_RTC_INSTALL_PREFIX@/lib/mc_robots"});
+
+  // clang-format off
+  auto rm = mc_rbdyn::RobotLoader::get_robot_module(@ROBOT_MODULE_PARAMS@);
+  // clang-format on
+
   return 0;
 }
